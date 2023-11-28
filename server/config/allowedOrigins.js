cd1275772e4f79119131c8cfcb94d5c8d8ca4274
adam <adamwoods124@gmail.com>
2023-11-27 22:37:00 -0800

    backend deployment ready

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+    'https://aw-medapp.onrender.com'
 ]
 
 module.exports = allowedOrigins
--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
+        if (allowedOrigins.includes(origin)) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
